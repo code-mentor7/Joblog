15fd9e46176ff5e774a3baf20d7f9b296b5d6d25
jinjinsu <jjs19930421@gmail.com>
2019-03-30 03:12:52 +0800

    fetch job log data from BE and show them

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,55 @@
 import React, { Component } from 'react';
 
-const list = [
-  {
-    id: 1,
-    title: '1st Item',
-    description: 'Description here.'
-  },
-  {
-    id: 2,
-    title: '2nd Item',
-    description: 'Another description here.'
-  },
-  {
-    id: 3,
-    title: '3rd Item',
-    description: 'Third description here.'
-  }
-];
-
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { list };
+    this.state = { list: [] };
+  }
+
+  componentDidMount() {
+    fetch(`http://localhost:8000/api/get`)
+        .then(result=>result.json())
+        .then(items=>this.setState({list: items}))
   }
 
   render() {
     return (
-      <div>
-        {this.state.list.map(item => (
-          <div>
-            <h1>{item.title}</h1>
-            <span>{item.description}</span>
-          </div>
-        ))}
+      <div style={{paddingTop: '100px'}}>
+        <table className="table table-striped">
+          <thead className="text-center">
+            <tr>
+              <th>JOB_LOG_PK</th>
+              <th>IS_COMPLETE</th>
+              <th>SUBMIT_DT</th>
+              <th>XPR_JOB_ID</th>
+              <th>OVRD_FILE_PATH</th>
+              <th>JOB_STATUS</th>
+              <th>API_JOB_NAME</th>
+              <th>X_RUN_ID</th>
+              <th>X_PROCESS_ID</th>
+              <th>X_LOG_PATH</th>
+              <th>X_SERVER</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.list.map(item => (
+                <tr>
+                  <td className="text-center">{item.JOB_LOG_PK}</td>
+                  <td className="text-center">{item.IS_COMPLETE}</td>
+                  <td className="text-center">{item.SUBMIT_DT}</td>
+                  <td className="text-center">{item.XPR_JOB_ID}</td>
+                  <td className="text-center">{item.OVRD_FILE_PATH}</td>
+                  <td className="text-center">{item.JOB_STATUS}</td>
+                  <td className="text-center">{item.API_JOB_NAME}</td>
+                  <td className="text-center">{item.X_RUN_ID}</td>
+                  <td className="text-center">{item.X_PROCESS_ID}</td>
+                  <td className="text-center">{item.X_LOG_PATH}</td>
+                  <td className="text-center">{item.X_SERVER}</td>
+                </tr>
+            ))}
+          </tbody>
+        </table>
+
       </div>
     );
   }
